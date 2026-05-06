d24caa0a92f17607537ed2f244f8cab4f004c4c6
atomer774-wq <atomer774@gmail.com>
2026-05-06 16:58:51 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,16 @@
+const products = [
+    {
+        model: "TV001",
+        mrp: "₹25000",
+        ean: "123456",
+        dimension: "40 inch",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        model: "MOB001",
+        mrp: "₹15000",
+        ean: "654321",
+        dimension: "6 inch",
+        image: "https://via.placeholder.com/150"
+    }
+];
